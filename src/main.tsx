b499171e4f20085a3f82097940854239ebe73b98
Danilhad <hadiullin2001@gmail.com>
2025-09-06 10:57:45 +0300

    Исправления шрифта

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import { AppRoot } from '@telegram-apps/telegram-ui'
 import App from './App'
 
+// Применяем шрифты глобально
+document.documentElement.style.setProperty('--tgui--font-family', 
+    '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, sans-serif');
+
 // Импортируйте стили Telegram UI
 import '@telegram-apps/telegram-ui/dist/styles.css'
 // import './App.css'
